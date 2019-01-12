bfb8bd84d69eb408e75d9c566941026b2f06e86c
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-12 00:04:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,12 @@
 class Artist
   attr_accessor :name
-
+  def initialize
+    @songs = []
+  end
+  
+  def add_song(title)
+    song = Song.new(title)
+    @songs << song
+    song.artist = self
+  end
 end
