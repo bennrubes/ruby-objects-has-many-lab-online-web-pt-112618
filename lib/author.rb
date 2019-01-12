c22e71c88fdfdb9462eef5fd9e397d0bbf17f490
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-12 00:46:24 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,7 +10,7 @@ class Author
     new_post.author = self
   end
   
-  def add_post_by_name(title)
+  def add_post_by_title(title)
     new_post = Post.new(title)
     @@posts << new_post
     new_post.artist = self
