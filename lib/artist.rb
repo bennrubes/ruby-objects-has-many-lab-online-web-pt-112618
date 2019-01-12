5d7f01cd0fc38476657dc2e3a30a7fdbc35f40f9
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-12 00:37:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,25 @@
 class Artist
   attr_accessor :name
+  @@songs
+  
   def initialize(name)
     @name = name
-    @@songs = []
+    @songs = []
   end
   
   def add_song(song)
-    @@songs << song
+    @songs << song
     song.artist = self
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
-    @@songs << song
+    @songs << song
     song.artist = self
   end
   
   def songs
-    @@songs
+    @songs
   end
   
   def self.song_count
