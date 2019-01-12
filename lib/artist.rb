45ae78b6e02d202f3a41be610c90ebc17bedb0d1
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-12 00:39:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,12 +9,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@songs << song
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@songs << song
   end
   
   def songs
