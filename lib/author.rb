217b0610170d20a0fe555080490235b0d966d74c
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-12 00:47:02 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,23 +1,27 @@
 class Author
   attr_accessor :name
+  @@posts = []
+  
   def initialize(name)
     @name = name
-    @@posts = []
+    @posts = []
   end
   
   def add_post(new_post)
-    @@posts << new_post
+    @posts << new_post
     new_post.author = self
+    @@posts << new_post
   end
   
   def add_post_by_title(title)
     new_post = Post.new(title)
-    @@posts << new_post
+    @posts << new_post
     new_post.artist = self
+    @@posts << new_post
   end
   
   def posts
-    @@posts
+    @posts
   end
   
   def self.post_count
