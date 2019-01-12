0af071346423994cb42f058e5d332fadf8099db2
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-12 00:51:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,17 @@
 class Song
   attr_accessor :name, :artist
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    self.author.name
   end
 end
