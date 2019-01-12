e1ede43b36d647087775eda8733367dbe56bb0af
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-12 00:29:10 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,16 +1,26 @@
 class Author
   attr_accessor :name
-  def initialize
-    @posts = []
+  def initialize(name)
+    @name = name
+    @songs = []
   end
   
-  def add_new_post(title)
-    new_post = Song.new(title)
-    @posts << new_post
-    new_post.author = self
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
   
-  def posts
-    @posts
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def self.song_count
+    @songs.count
   end
 end
